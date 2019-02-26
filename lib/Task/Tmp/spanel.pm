09a112744a17ef8d99a49344a5de5b62d5afdc34
perlancar (on pc-office) <perlancar@gmail.com>
2019-02-26 15:13:48 +0700

    Initial commit

---
+++ lib/Task/Tmp/spanel.pm
@@ -0,0 +1,227 @@
+package Task::Tmp::spanel;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Perl modules for Spanel
+
+=pkggroup Included modules
+
+=pkg APR::Table
+
+=pkg Algorithm::Dependency::Ordered
+
+=pkg Algorithm::Dependency::Source::HoA
+
+=pkg Any::Moose
+
+=pkg Apache2::Access
+
+=pkg Apache2::CmdParms
+
+=pkg Apache2::Connection
+
+=pkg Apache2::Const
+
+=pkg Apache2::Directive
+
+=pkg Apache2::Log
+
+=pkg Apache2::Module
+
+=pkg Apache2::RequestIO
+
+=pkg Apache2::RequestRec
+
+=pkg Apache2::RequestUtil
+
+=pkg Apache2::ServerRec
+
+=pkg CDB_File
+
+=pkg CHI
+
+=pkg Capture::Tiny
+
+=pkg Carp
+
+=pkg Clone::Any
+
+=pkg Complete::Util
+
+=pkg Config::IniFiles
+
+=pkg Crypt::PasswdMD5
+
+=pkg Cwd
+
+=pkg DBD::mysql
+
+=pkg DBI
+
+=pkg Data::Clone
+
+=pkg Data::Dump::PHP
+
+=pkg Data::Dumper
+
+=pkg Data::ModeMerge
+
+=pkg Data::Sah
+
+=pkg Date::Manip
+
+=pkg DateTime
+
+=pkg Digest::MD5
+
+=pkg Digest::SHA1
+
+=pkg Error
+
+=pkg Exporter
+
+=pkg Fcntl
+
+=pkg File::Copy::Recursive
+
+=pkg File::Find
+
+=pkg File::Flock
+
+=pkg File::Path
+
+=pkg File::Slurp
+
+=pkg File::Slurp::Shortcuts
+
+=pkg File::Slurp::Tiny
+
+=pkg File::Spec
+
+=pkg File::Temp
+
+=pkg File::Which
+
+=pkg File::chdir
+
+=pkg File::chmod
+
+=pkg FindBin
+
+=pkg GDBM_File
+
+=pkg Getopt::Long
+
+=pkg IO::CaptureOutput
+
+=pkg IO::File
+
+=pkg IO::Prompt::I18N
+
+=pkg IO::Socket::UNIX
+
+=pkg IPC::System::Options
+
+=pkg LWP::UserAgent
+
+=pkg List::MoreUtils
+
+=pkg List::Util
+
+=pkg Log::Log4perl
+
+=pkg Log::ger
+
+=pkg MIME::Base64
+
+=pkg Mail::Address
+
+=pkg Mail::Sendmail
+
+=pkg Module::List
+
+=pkg Module::Load
+
+=pkg Module::Loaded
+
+=pkg Module::Version
+
+=pkg Moo
+
+=pkg Moo::Role
+
+=pkg Net::DNS
+
+=pkg Net::Domain::TLD
+
+=pkg POSIX
+
+=pkg Path::Class
+
+=pkg Perinci::Access::HTTP::Client
+
+=pkg Perinci::CmdLine
+
+=pkg Perinci::CmdLine::Lite
+
+=pkg Perinci::CmdLine::Server
+
+=pkg Perinci::Exporter
+
+=pkg Perinci::Sub::Util
+
+=pkg Proc::ChildError
+
+=pkg Proc::PID::File
+
+=pkg Quota
+
+=pkg RRDTool::OO
+
+=pkg Regexp::Shellish
+
+=pkg SHARYANTO::Template::Util
+
+=pkg Scalar::Util
+
+=pkg Storable
+
+=pkg String::ShellQuote
+
+=pkg Sub::Curry
+
+=pkg Time::Local
+
+=pkg URI
+
+=pkg URI::Escape
+
+=pkg UUID::Random
+
+=pkg WWW::Mechanize
+
+=pkg YAML::XS
+
+=pkg autodie
+
+=pkg base
+
+=pkg bytes
+
+=pkg constant
+
+=pkg experimental
+
+=pkg feature
+
+=pkg lib
+
+=pkg strict
+
+=pkg vars
+
+=pkg warnings
+
+=cut
